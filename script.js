d9e31a96d66703733ea355d60339364887935252
Echo Jicha <echojicha@gmail.com>
2021-06-22 18:38:07 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
+function compute() {
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("textInput").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+      if (isNaN(principal) || principal < 1 ) {
+    alert("Enter a positive number");
+    document.getElementById("principal").focus();
+  } else {
+    document.getElementById("result").innerHTML = "If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+ rate +"%</mark>\<br\>You will receive an amount of <mark>"+ interest +"</mark>,\<br\>in the year <mark>"+ year +"</mark>\<br\>";
+  }
     
 }
-        
+function updateTextInput(val) {
+          document.getElementById('textInput').value=val; 
+}        
